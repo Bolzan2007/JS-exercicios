ac10903baa12fafe89f8211619ee5faa73627b3f
Pietro Bolzan <137586050+Bolzan2007@users.noreply.github.com>
2023-11-22 16:49:00 -0300

    Add files via upload

---
+++ msc.js
@@ -0,0 +1,57 @@
+
+// let nome
+// let cidade
+
+// console.log(typeof nome);
+// console.log(typeof cidade); 
+
+// nome = prompt("Digite seu nome:");
+// cidade = prompt("Digite a cidade onde você mora:");
+
+// console.log(typeof nome); 
+// console.log(typeof cidade); 
+
+// console.log("Olá, meu nome é " + nome + " e eu moro em " + cidade + ".");
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// // Pergunta 1
+// let resposta1 = prompt("Você gosta de programar? (Responda com Sim ou Não)");
+
+// // Pergunta 2
+// let resposta2 = prompt("Você já viajou para fora do país? (Responda com Sim ou Não)");
+
+// // Pergunta 3
+// let resposta3 = prompt("Você tem animais de estimação? (Responda com Sim ou Não)");
+
+// // Imprimir as perguntas e respostas
+// console.log("Pergunta 1: Você gosta de programar? Resposta: " + resposta1);
+// console.log("Pergunta 2: Você já viajou para fora do país? Resposta: " + resposta2);
+// console.log("Pergunta 3: Você tem animais de estimação? Resposta: " + resposta3);
+
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// let x = 5 
+// let y = 10
+// let z
+
+// z = x
+// x = y
+// y = z
+
+// console.log("Troquei valores x = 10", x)
+// console.log("Troquei valores y = 5", y)
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+// let numero1 = Number(prompt (' Insira seu primeiro número'))
+// let numero2 = Number(prompt (' Insira seu segundo número'))
+ 
+// let soma = numero1 + numero2
+// console.log(soma)
+ 
+// let multiplicar = numero1*numero2
